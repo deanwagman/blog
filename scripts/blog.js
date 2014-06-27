0f9b93b25cf660f261f177d75205ce5fd9561f90
Dean.Wagman <deanwagman@gmail.com>
2014-06-27 17:00:44 -0400

    Add JS file, Fade-in scroll effect

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,9 +1,38 @@
 /* blog.js */
 
+var win = $(window);
+
+//if the articles scrollHeight is more than the scrolltop + wh - 200px, or less than scrolltop + 200px then blur.
+
+var articles = $('article');
+
+function updateBlur(){
+	console.log("scrolled");
+	var scrollPosition = win.scrollTop();
+	articles.each(function(article){
+		if(article.position().top >  scrollPosition - 200){
+			article.addClass('blurred');
+		}
+	});
+}
+
+$(window).on('scroll', function(){
+	console.log("scrolled");
+	articles.each(function(index){
+		var offset = $(this).offset();
+		if(offset.top >  $(window).scrollTop() + window.innerHeight - (window.innerHeight * 0.15)){
+			$(this).addClass('inverted');
+		}else{
+			$(this).removeClass('inverted');
+		}
+	});
+});
+
+
 
 
 $('img').on('click', function(){
-	var src, img, box;
+	var src, img, box, close;
 	src = $(this).attr('src');
 	img = $("<img></img>").attr('src', src);
 	box = $('.lightbox')
@@ -14,5 +43,11 @@ $('img').on('click', function(){
 		box.removeClass('active');
 	});
 });
+
+$('.blurred').on('click',function(){
+	$(this).toggleClass('blurred');
+});
+
+
 
 
