f1f0249f6fc157aead571a5d4a771d804e15b146
Dean.Wagman <deanwagman@gmail.com>
2014-06-27 20:48:20 -0400

    Refactor code yo

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,52 +1,41 @@
 /* blog.js */
+'use strict';
 
-var win = $(window);
+// Global Variables
+var win, articles;
+win = $(window);
+articles = $('article');
 
-//if the articles scrollHeight is more than the scrolltop + wh - 200px, or less than scrolltop + 200px then blur.
-
-var articles = $('article');
-
-function updateBlur(){
-	console.log("scrolled");
-	var scrollPosition = win.scrollTop();
-	articles.each(function(article){
-		if(article.position().top >  scrollPosition - 200){
-			article.addClass('blurred');
-		}
-	});
-}
-
-$(window).on('scroll', function(){
-	console.log("scrolled");
-	articles.each(function(index){
-		var offset = $(this).offset();
-		if(offset.top >  $(window).scrollTop() + window.innerHeight - (window.innerHeight * 0.15)){
-			$(this).addClass('inverted');
-		}else{
-			$(this).removeClass('inverted');
-		}
-	});
+// Scroll Effect that fades in Articles as they come into View
+win.on('scroll', function () {
+    articles.each(function () {
+        // Cache each articles coordinates
+        var article, offset;
+        article = $(this);
+        offset = article.offset();
+        // Conditional to see if article's 15% in view
+        if (offset.top >  win.scrollTop() + win.height() - (win.height() * 0.25)) {
+            article.addClass('transparent');
+        } else {
+            article.removeClass('transparent');
+        }
+    });
 });
 
-
-
-
-$('img').on('click', function(){
-	var src, img, box, close;
-	src = $(this).attr('src');
-	img = $("<img></img>").attr('src', src);
-	box = $('.lightbox')
-		.empty()
-		.append(img)
-		.addClass('active');
-	box.on('click',function(){
-		box.removeClass('active');
-	});
+// Light Box Functionality
+$('img').on('click', function () {
+    var src, img, box;
+    src = $(this).attr('src');
+    img = $("<img></img>").attr('src', src);
+    box = $('.lightbox')
+        .empty()
+        .append(img)
+        .addClass('active');
+    box.on('click', function () {
+        box.removeClass('active');
+    });
 });
 
-$('.blurred').on('click',function(){
-	$(this).toggleClass('blurred');
-});
 
 
 
