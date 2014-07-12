1aa7d732ae5e150dfb251a4326efb3b4c1bf53ab
Dean.Wagman <deanwagman@gmail.com>
2014-07-12 16:36:47 -0400

    Fix jQuery scope demand in WordPress

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,40 +1,45 @@
-/* blog.js */
+
 'use strict';
 
-// Global Variables
-var win, articles;
-win = $(window);
-articles = $('article');
+jQuery(document).ready(function ($) {
+    // Variables
+    var win, articles;
+    win = $(window);
+    articles = $('article');
 
-// Scroll Effect that fades in Articles as they come into View
-win.on('scroll', function () {
-    articles.each(function () {
-        // Cache each articles coordinates
-        var article, offset;
-        article = $(this);
-        offset = article.offset();
-        // Conditional to see if article's 15% in view
-        if (offset.top >  win.scrollTop() + win.height() - (win.height() * 0.25)) {
-            article.addClass('transparent');
-        } else {
-            article.removeClass('transparent');
-        }
+    // Scroll Effect that fades in Articles as they come into View
+    win.on('scroll', function () {
+        articles.each(function () {
+            // Cache each articles coordinates
+            var article, offset;
+            article = $(this);
+            offset = article.offset();
+            // Conditional to see if article's 15% in view
+            if (offset.top >  win.scrollTop() + win.height() - (win.height() * 0.25)) {
+                article.addClass('transparent');
+            } else {
+                article.removeClass('transparent');
+            }
+        });
     });
+
+    // Light Box Functionality
+    $('img').on('click', function () {
+        var src, img, box;
+        src = $(this).attr('src');
+        img = $("<img></img>").attr('src', src);
+        box = $('.lightbox')
+            .empty()
+            .append(img)
+            .addClass('active');
+        box.on('click', function () {
+            box.removeClass('active');
+        });
+    });
+
 });
 
-// Light Box Functionality
-$('img').on('click', function () {
-    var src, img, box;
-    src = $(this).attr('src');
-    img = $("<img></img>").attr('src', src);
-    box = $('.lightbox')
-        .empty()
-        .append(img)
-        .addClass('active');
-    box.on('click', function () {
-        box.removeClass('active');
-    });
-});
+
 
 
 
